4947c4d330ef70ab0ad7d0992b5a68f36720a9ef
thisd92 <80495413+thisd92@users.noreply.github.com>
2023-01-12 00:47:06 -0300

    create function clearAll

--- index.js
+++ index.js
@@ -2,16 +2,19 @@ const inputItem = document.getElementById("inputItem");
 const btnSubmit = document.getElementById("btnSubmit");
 const divAlert = document.getElementById("divAlert");
 const divList = document.getElementById("container-list");
+const btnClear = document.getElementById("btnClear");
+const btnClearAll = document.getElementById("clearAll");
+const cardList = document.getElementById("cardList")
 
-const successMsg = "<div id='alert' class='divAlert success'><span>" + "Item adicionado com sucesso!" + "</span></div>";
 const errorMsg = "<div id='alert' class='divAlert error'><span>Por favor, preencha o campo para adicionar um item!</span></div>";
 
-
-btnSubmit.addEventListener('click', addItem)
+btnSubmit.addEventListener('click', addItem);
+btnClearAll.addEventListener('click', clearAll);
 
 function addItem(e) {
     e.preventDefault()
     validateForm();
+    
 }
 
 
@@ -20,15 +23,19 @@ function validateForm() {
     showMessage(item)
     if (item) {
         const itemList = document.createElement("div");
+        itemList.classList.add("item");
         clearInput();
         itemList.innerHTML = `
-        <p>${item}</p><div><button class='clearBtn'><i class="fas fa-trash"></i></button></div>`
+        <p>${item}</p><div><button id='btnClear' onclick='deleteItem()' class='clearBtn'><i class="fas fa-trash"></i></button></div>`
         divList.appendChild(itemList);
+        btnClearAll.style.visibility = "visible";
+        addLocalStorage(item);
     }
-};
+}
 
 const showMessage = (hasItem) => {
     if (hasItem) {
+        const successMsg = `<div id='alert' class='divAlert success'><span>${hasItem} adicionado com sucesso!</span></div>`;
         divAlert.innerHTML = successMsg;
         setTimeout(() => {
             divAlert.removeChild(divAlert.firstChild);
@@ -38,14 +45,31 @@ const showMessage = (hasItem) => {
         setTimeout(() => {
             divAlert.removeChild(divAlert.firstChild);
         }, 3000);
-
+        
     }
-};
+}
 
 function clearInput() {
     inputItem.value = "";
 }
 
 function deleteItem() {
+    console.log("funciona");
+}
 
+function clearAll(){
+    const items = document.querySelectorAll(".item");
+    
+    if(items.length > 0){
+        items.forEach((item) => {
+            divList.removeChild(item);
+        });
+    }
+
+    btnClearAll.style.visibility = "hidden";
+    // localStorage.removeItem('list');
+}
+
+function addLocalStorage(item){
+    console.log("added");
 }
